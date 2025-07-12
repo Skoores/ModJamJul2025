f2123f663a7fb32ee295cb6310dddb10ae9137b0
Gepapa363 <63466328+Gepapa363@users.noreply.github.com>
2025-07-12 17:59:33 +0300

    Add files via upload

---
+++ NPC/Galacta.cs
@@ -0,0 +1,78 @@
+using ModJamJul2025.Systems;
+using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using Terraria;
+using Terraria.Audio;
+using Terraria.GameContent.Bestiary;
+using Terraria.GameContent.ItemDropRules;
+using Terraria.Graphics.CameraModifiers;
+using Terraria.ID;
+using Terraria.Localization;
+using Terraria.ModLoader;
+
+namespace ModJamJul2025.NPC.Galacta
+{
+    [AutoloadBossHead]
+    public class GalactaKnight : ModNPC
+    {
+        public override void SetStaticDefaults()
+        {
+            NPCID.Sets.BossBestiaryPriority.Add(Type);
+            NPCID.Sets.SpecificDebuffImmunity[Type][BuffID.Confused] = true;
+        }
+
+        public override void SetDefaults()
+        {
+            NPC.width = 60;
+            NPC.height = 76;
+            NPC.damage = 12;
+            NPC.defense = 100;
+            NPC.lifeMax = 150000;
+            NPC.HitSound = SoundID.NPCHit1;
+            NPC.DeathSound = SoundID.NPCDeath1;
+            NPC.knockBackResist = 0f;
+            NPC.noGravity = true;
+            NPC.noTileCollide = true;
+            NPC.SpawnWithHigherTime(30);
+            NPC.boss = true;
+            NPC.npcSlots = 10f;
+
+            NPC.aiStyle = -1;
+
+            //boss bar goes here once we make it
+
+            if (!Main.dedServ)
+            {
+                Music = MusicLoader.GetMusicSlot(Mod, "Assets/Music/Ropocalypse2");
+            }
+        }
+
+        public override void ModifyNPCLoot(NPCLoot npcLoot)
+        {
+            //no bitches?
+        }
+
+        public override void OnKill()
+        {
+            NPC.SetEventFlagCleared(ref DownedBossSystem.resealedGalactaKnight, -1);
+        }
+
+        public override void BossLoot(ref int potionType)
+        {
+            potionType = ItemID.SuperHealingPotion;
+        }
+
+        public override bool CanHitPlayer(Player target, ref int cooldownSlot)
+        {
+            cooldownSlot = ImmunityCooldownID.Bosses;
+            return true;
+        }
+
+        public override void AI()
+        {
+            
+        }
+    }
+
+}
---
+++ Systems/DownedBossSystem.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.IO;
+using Terraria;
+using Terraria.ModLoader;
+using Terraria.ModLoader.IO;
+namespace ModJamJul2025.Systems
+{
+    public class DownedBossSystem : ModSystem
+    {
+        public static bool resealedGalactaKnight = false;
+        public override void ClearWorld()
+        {
+            resealedGalactaKnight = false;
+        }
+
+        public override void SaveWorldData(TagCompound tag)
+        {
+            if (resealedGalactaKnight)
+            {
+                tag["resealedGalactaKnight"] = true;
+            }
+        }
+
+        public override void LoadWorldData(TagCompound tag) {
+			resealedGalactaKnight = tag.ContainsKey("resealedGalactaKnight");
+		}
+
+        public override void NetSend(BinaryWriter writer) {
+			writer.WriteFlags(resealedGalactaKnight);
+			}
+
+		public override void NetReceive(BinaryReader reader) {
+			reader.ReadFlags(out resealedGalactaKnight);
+		}
+    }
+}
