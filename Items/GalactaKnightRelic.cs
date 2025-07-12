3d166b815a18217234222cff8549429446fe99ac
Skoores <philliprake04@gmail.com>
2025-07-12 15:15:13 -0400

    misc galacta stuff

---
+++ Items/GalactaKnightRelic.cs
@@ -0,0 +1,22 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using ModJamJul2025.Tiles;
+
+namespace ModJamJul2025.Items
+{
+    public class GalactaKnightRelic : ModItem, ILocalizedModType
+    {
+        public new string LocalizationCategory => "Items.Placeable";
+        public override void SetDefaults()
+        {
+            Item.DefaultToPlaceableTile(ModContent.TileType<GalactaKnightRelicTile>());
+
+            Item.width = 30;
+            Item.height = 40;
+            Item.rare = ItemRarityID.Master;
+            Item.master = true;
+            Item.value = Item.buyPrice(0, 5);
+        }
+    }
+}
--- obj/Debug/net8.0/ModJamJul2025.AssemblyInfo.cs
+++ obj/Debug/net8.0/ModJamJul2025.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ModJamJul2025")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6c0d17dd7fc4444d1fb93e655f79835a082f0106")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a59d7818c030d3adcd28a3a903111f04aec4f73f")]
 [assembly: System.Reflection.AssemblyProductAttribute("ModJamJul2025")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ModJamJul2025")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
